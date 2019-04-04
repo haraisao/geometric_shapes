13152627c02dde7bd194aee31906ae0369a0e819
Isao Hara <haraisao@gmail.com>
2019-04-04 15:24:19 +0900

    Update to search libs

--- cmake/FindQhull.cmake
+++ cmake/FindQhull.cmake
@@ -7,7 +7,6 @@
 # QHULL_LIBRARIES - Libraries needed to use QHULL.
 # If QHULL_USE_STATIC is specified then look for static libraries ONLY else 
 # look for shared ones
-
 set(QHULL_MAJOR_VERSION 6)
 
 if(QHULL_USE_STATIC)
@@ -18,11 +17,12 @@ else(QHULL_USE_STATIC)
   set(QHULL_DEBUG_NAME qhull_pd qhull${QHULL_MAJOR_VERSION}_d qhull_d${QHULL_MAJOR_VERSION} qhull_d)
 endif(QHULL_USE_STATIC)
 
+message("${CMAKE_PREFIX_PATH}")
 find_file(QHULL_HEADER
           NAMES libqhull/libqhull.h qhull.h
           HINTS "${QHULL_ROOT}" "$ENV{QHULL_ROOT}" "${QHULL_INCLUDE_DIR}"
           PATHS "$ENV{PROGRAMFILES}/QHull" "$ENV{PROGRAMW6432}/QHull" 
-          PATH_SUFFIXES qhull src/libqhull libqhull include)
+          PATH_SUFFIXES qhull src/libqhull libqhull include qhull/include)
 
 set(QHULL_HEADER "${QHULL_HEADER}" CACHE INTERNAL "QHull header" FORCE )
 
@@ -35,6 +35,11 @@ if(QHULL_HEADER)
     set(HAVE_QHULL_2011 ON)
     get_filename_component(QHULL_INCLUDE_DIR ${QHULL_HEADER} PATH)
     get_filename_component(QHULL_INCLUDE_DIR ${QHULL_INCLUDE_DIR} PATH)
+  endif()
+  if(WIN32)
+    if(NOT QHULL_ROOT)
+      get_filename_component(QHULL_ROOT "${QHULL_HEADER}/../../.." ABSOLUTE)
+    endif()
   endif()
 else(QHULL_HEADER)
   set(QHULL_INCLUDE_DIR "QHULL_INCLUDE_DIR-NOTFOUND")
